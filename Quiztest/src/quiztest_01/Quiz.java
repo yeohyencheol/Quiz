5c459eea7ac425397b0facbd67f62faf4a465004
admin <admin@DESKTOP-SM3E7CB>
2021-10-05 10:58:21 +0900

    메세지 두개 출력!

--- Quiztest/src/quiztest_01/Quiz.java
+++ Quiztest/src/quiztest_01/Quiz.java
@@ -5,5 +5,12 @@ public class Quiz {
 	private String name;
 	private int number;
 	
+	public static void main(String[] args) {
+		System.out.println("!!!!!!");
+		
+		System.out.println("@@@@@@@@");
+	}
+	
+	
 
 }
