82e7721345cafbd9eb0d9c485f222f12a947aa92
kr421 <kr421@221.138.219.182>
2021-10-06 10:00:11 +0900

    수정(2021/10/6 09:59)

--- Quiztest/src/quiztest_01/DeleteUser.java
+++ Quiztest/src/quiztest_01/DeleteUser.java
@@ -6,14 +6,29 @@ import java.util.Scanner;
 public class DeleteUser {
 	public static void main(String[] args) {
 		DbQuizDao dao = DbQuizDaoImpl.getInstance();
-		
+
 		Scanner scan = new Scanner(System.in);
 		System.out.println("<회월탈퇴>");
 		System.out.println("등록된 아이디를 입력하세요.");
 		int id = 0;
-		
-		id = scan.nextInt();
-		
+
+		while(true) {
+			id = scan.nextInt();
+
+			try {
+				User usercheck = dao.findById(id);
+				if (usercheck == null) {
+					System.out.println("등록되지 않은 아이디입니다. 다시 입력하세요.");
+				} else {
+					break;
+				}
+			} catch (ClassNotFoundException e1) {
+				e1.printStackTrace();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+
 		try {
 			dao.deleteUser(id);
 			System.out.println("회원탈퇴완료");
